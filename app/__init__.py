fe50200724f718d47a862908f59bcd555561e7d5
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-04-15 17:21:57 +0200

    feat: add socketIO for launching server

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_socketio import SocketIO
+from app.routes.users import users_bp
 
+
+socketio = SocketIO()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__)        
+
+    app.register_blueprint(users_bp)
+    socketio.init_app(app)
+    
     return app
